f9de003a2d5ad9973be122fff2da20dfe3741210
kugel3 <gustaf.kugelberg@rdx.works>
2023-09-12 17:51:30 +0200

    Add full scope

--- Sources/ComposableArchitecture/Store.swift
+++ Sources/ComposableArchitecture/Store.swift
@@ -675,6 +675,30 @@ public final class Store<State, Action> {
   public var publisher: StorePublisher<State> {
     StorePublisher(store: self, upstream: self.state)
   }
+
+	/// Scopes the store to one that exposes child state and actions.
+	///
+	/// A version of ``scope(state:action:)-9iai9`` that exposes the child state along
+	/// with the child action for scoping actions.
+	///
+	/// - Parameters:
+	///   - toChildState: A function that transforms `State` into `ChildState`.
+	///   - fromChildAction: A function that transforms `ChildState` and `ChildAction` into `Action`.
+	/// - Returns: A new store with its domain (state and action) transformed.
+	@_disfavoredOverload
+	@_spi(Internals) public func scope<ChildState, ChildAction>(
+		state toChildState: @escaping (State) -> ChildState,
+		action fromChildStateAndAction: @escaping (ChildState, ChildAction) -> Action
+	) -> Store<ChildState, ChildAction> {
+		self.threadCheck(status: .scope)
+
+		return self.reducer.rescope(
+			self,
+			state: toChildState,
+			action: { fromChildStateAndAction($0, $1) },
+			removeDuplicates: nil
+		)
+	}
 }
 
 /// A convenience type alias for referring to a store of a given reducer's domain.
